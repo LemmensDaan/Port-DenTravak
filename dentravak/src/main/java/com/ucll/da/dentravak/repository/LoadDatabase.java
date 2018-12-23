c2ac7fd757b2efd40e393786e9201b69274fe83e
fagenorn <anisanissakkaf@gmail.com>
2018-12-23 18:28:24 +0100

    Fixed consul using hostnames instead of ip address

--- dentravak/src/main/java/com/ucll/da/dentravak/repository/LoadDatabase.java
+++ dentravak/src/main/java/com/ucll/da/dentravak/repository/LoadDatabase.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-import java.time.LocalDateTime;
 
 @Component
 class LoadDatabase implements ApplicationRunner {
